41c2e1937f112c25f9310d1013023cf5c48e4159
ClayGuess <guessclay@Yahoo.com>
2020-07-08 17:09:39 -0400

    debugging

--- providence_project/settings.py
+++ providence_project/settings.py
@@ -17,9 +17,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Add these two lines.
 REACT_APP_DIR = os.path.join(BASE_DIR, 'client')
 
-STATIC_URL = '/static/' 
-STATIC_ROOT = '/home/mysite/static/'
- 
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'build', 'static')
 ]
